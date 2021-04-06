9e21cf9383a1454c087e19e84cf8f28f030b35bf
Harpreet Bains <37309359+hnybains@users.noreply.github.com>
2021-04-06 01:35:12 -0600

    changes

--- admin/AddNewStudent.php
+++ admin/AddNewStudent.php
@@ -1,6 +1,6 @@
 <?php include 'userinfo_admin.php'; ?>
 <?php
- $db = pg_connect("host=thetechphantoms-do-user-8660169-0.b.db.ondigitalocean.com port=25060 dbname=Postgres user=CMHA password=j38mp49ya50ow9im");
+ $db = pg_connect("sslmode=require sslrootcert=./certificates/ca-certificate.crt host=thetechphantoms-do-user-8660169-0.b.db.ondigitalocean.com port=25060 dbname=Postgres user=CMHA password=j38mp49ya50ow9im");
  if (!$db){  
   echo "<center><h1>Doesn't work =(</h1></center>";  
   }else

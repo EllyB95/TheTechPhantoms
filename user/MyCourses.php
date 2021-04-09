6639bd7e837602c28a5a91f8b19217f0dc6a275e
Harpreet Bains <37309359+hnybains@users.noreply.github.com>
2021-04-09 00:23:29 -0600

    Update MyCourses.php

--- user/MyCourses.php
+++ user/MyCourses.php
@@ -35,77 +35,109 @@
 </head>
 
 <body>
-  <header class="page-header">
-     <?php include 'LeftMenu.php'; ?>
-  </header>
+  <div class="row">
+    <div class='col-3'>
+      <header class="page-header">
+        <?php include 'LeftMenu.php'; ?>
 
-  <section class="page-content">
-      <section class="grid">
-          <div class="main__container">
-              <div class="main__title">
-                  <div class="main__greeting">
-
-                        <?php include '../config.php';?>
-                        <?php
-                        $sql = pg_query(sprintf("SELECT * FROM public.enroll where emailaddress ='" . pg_escape_string($_SESSION['EmailStudent']) . "';"));
-                        $count = 0;
-
-                        while ($row = pg_fetch_assoc($sql)) {
-                          $count = $count + 1;
-
-                          $sql2 = pg_fetch_assoc(pg_query(sprintf("SELECT * FROM public.courses where course_id = " . $row['course_id'] . ";")));
-                          echo "
-                                  <div class='col-12 col-md-6 card-container'>
-                                    <div id='tribe-event-content--5068' class='card tribe-events-single events-single-card' data-filter-container=''>
-                                      <div class='location-meta' data-location='online'></div>
-                                      <div class='tags' data-filter-target='' data-tags='online'></div>
-                                      <div class='card__header'>
-                                        <div class='card__title title-4 tribe-events-single-event-title'>" . htmlspecialchars($sql2['course_name']) . "</div> 
-                                        </div>
-
-                                      <div class='card__body small'>
-                                      
-
-                                        <table class='details'>
-                                          <tbody>
-                                            <tr>
-                                              <th>Start</th>
-                                              <td>" . htmlspecialchars($sql2['start_date']) . "</td>
-                                            </tr>
-                                            <tr>
-                                              <th>End</th>
-                                              <td>" . htmlspecialchars($sql2['end_date']) . "</td>
-                                            </tr>
-                                            <tr>
-                                            <th>Description</th>
-                                            <td>" . htmlspecialchars($sql2['description']) . "</td>
-                                          </tr>
-                                          </tbody>
-                                        </table>
-                                      </div>
-
-                                      <div class='card__footer'>
-                                        <button class='btn btn-primary' id='" . $row['course_id'] . "' onclick='startLearning(this.id)'>Start Learning</button>
-                                        <button class='btn btn-primary' id='" . $row['course_id'] . "' onclick='deleteCourse(this.id)'>Delete Course</button>
-                                      </div>
-                                    </div><!-- #tribe-events-content -->
-                                  </div>";
-                        }
-
-                        echo "<br><br>";
-
-                        if ($count == 0)
-                              echo "<center><p>You have not enrolled any courses</p></center>";
+      </header>
+    </div>
+    <div class='col-9'>
+      <div id="page" class="site">
+        <div id="content" class="site-content">
 
 
-                        ?>
 
-                      </div>
 
-                      </div>
-                    </div>
-                  </section>
-        </section>  
+
+
+          <div id="skip-anchor" tabindex="-1"></div>
+
+
+
+
+
+          <section class="upcoming-courses">
+
+            <div class="container">
+              <div class="row">
+
+
+
+                <?php include '../config.php';?>
+                <?php
+                $sql = pg_query(sprintf("SELECT * FROM public.enroll where emailaddress ='" . pg_escape_string($_SESSION['EmailStudent']) . "';"));
+                $count = 0;
+
+                while ($row = pg_fetch_assoc($sql)) {
+                  $count = $count + 1;
+
+                  $sql2 = pg_fetch_assoc(pg_query(sprintf("SELECT * FROM public.courses where course_id = " . $row['course_id'] . ";")));
+                  echo "
+                          <div class='col-12 col-md-6 card-container'>
+                            <div id='tribe-event-content--5068' class='card tribe-events-single events-single-card' data-filter-container=''>
+                              <div class='location-meta' data-location='online'></div>
+                              <div class='tags' data-filter-target='' data-tags='online'></div>
+                              <div class='card__header'>
+                                <div class='card__title title-4 tribe-events-single-event-title'>" . htmlspecialchars($sql2['course_name']) . "</div> 
+                                </div>
+
+                              <div class='card__body small'>
+                              
+
+                                <table class='details'>
+                                  <tbody>
+                                    <tr>
+                                      <th>Start</th>
+                                      <td>" . htmlspecialchars($sql2['start_date']) . "</td>
+                                    </tr>
+                                    <tr>
+                                      <th>End</th>
+                                      <td>" . htmlspecialchars($sql2['end_date']) . "</td>
+                                    </tr>
+                                    <tr>
+                                    <th>Description</th>
+                                    <td>" . htmlspecialchars($sql2['description']) . "</td>
+                                  </tr>
+                                  </tbody>
+                                </table>
+                              </div>
+
+                              <div class='card__footer'>
+                                <button class='btn btn-primary' id='" . $row['course_id'] . "' onclick='startLearning(this.id)'>Start Learning</button>
+                                <button class='btn btn-primary' id='" . $row['course_id'] . "' onclick='deleteCourse(this.id)'>Delete Course</button>
+                              </div>
+                            </div><!-- #tribe-events-content -->
+                          </div>";
+                }
+
+                echo "</div><br><br>";
+
+                if ($count == 0)
+                      echo "<center><p>You have not enrolled any courses</p></center>";
+
+
+                ?>
+
+              </div>
+
+              
+
+          </section>
+
+
+
+
+
+        </div><!-- #content -->
+
+
+
+      </div><!-- #page -->
+    </div>>
+  </div>
+
+
 </body>
 <footer class="page-footer">
 </footer>

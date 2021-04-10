ad18b2a4dff95e955b2b0775f45ead35301acfb9
Daman Bhardwaj <dbhardwaj2@nait.ca>
2021-04-10 03:14:38 -0600

    Update StudentChangePassword.php

--- user/StudentChangePassword.php
+++ user/StudentChangePassword.php
@@ -113,11 +113,10 @@ pg_close($dbconn);
 
 
 <!DOCTYPE html>
-<!DOCTYPE html>
 <html lang="en-CA" class="no-js">
 
 <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
   <link href='../css/studentStyle.css' rel='stylesheet' type="text/css" />
   <link href='../css/admin_table.css' rel='stylesheet' type="text/css" />

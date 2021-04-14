62ec8f6edce30a37ffe84d18723b3b68a06ffbbb
Daman Bhardwaj <dbhardwaj2@nait.ca>
2021-04-14 14:51:27 -0600

    Update EditMyAccount.php

--- admin/EditMyAccount.php
+++ admin/EditMyAccount.php
@@ -63,14 +63,19 @@ pg_close($db);
 ?>
 <!DOCTYPE html>
 <html lang="en-CA" class="no-js">
+
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-<link href='../css/studentStyle.css' rel='stylesheet' type="text/css"/>
-<link href='../css/admin_table.css' rel='stylesheet' type="text/css"/>
-<script src="https://code.iconify.design/1/1.0.7/iconify.min.js"></script>
-<svg style="display:none;">
-</svg>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  <link href='../css/studentStyle.css' rel='stylesheet' type="text/css" />
+  <link href='../css/admin_table.css' rel='stylesheet' type="text/css" />
+  <script src="https://code.iconify.design/1/1.0.7/iconify.min.js"></script>
+
+  <svg style="display:none;">
+  </svg>
+  <link rel="stylesheet" id="bootstrap-css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css?ver=67c90ffd8417a442ac33ffaa4a4ee97a" type="text/css" media="all">
+  <link rel="stylesheet" id="site_styles-css" href="../css/main_styles.css?ver=1.7" type="text/css" media="all">
+
 </head>
 
 <body>
